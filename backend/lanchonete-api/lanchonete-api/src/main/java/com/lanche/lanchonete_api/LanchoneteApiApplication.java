044a890cf334485ff0b03a351a66b4e1308a185a
Matheus Renato <teus.rbe06@gmail.com>
2026-04-21 09:10:51 -0300

    feat: estrutura inicial do backend com Spring Boot

---
+++ backend/lanchonete-api/lanchonete-api/src/main/java/com/lanche/lanchonete_api/LanchoneteApiApplication.java
@@ -0,0 +1,13 @@
+package com.lanche.lanchonete_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchoneteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchoneteApiApplication.class, args);
+	}
+
+}
---
+++ backend/lanchonete-api/lanchonete-api/src/test/java/com/lanche/lanchonete_api/LanchoneteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanche.lanchonete_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchoneteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
